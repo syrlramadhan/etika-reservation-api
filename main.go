243714c4400c29948392d6177b1b71e8f1289ae7
syrlramadhan <syahrul0422@gmail.com>
2025-06-20 04:17:14 +0800

    change origin

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://192.168.1.24")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
