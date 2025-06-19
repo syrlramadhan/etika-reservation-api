4a91a96ddd4a0cbb84b3ef509d31c7020159dd41
syrlramadhan <syahrul0422@gmail.com>
2025-06-20 00:24:14 +0800

    add cors middleware

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"net/http"
-
 	"log"
+	"net/http"
 
 	"github.com/syrlramadhan/etika-reservation-api/config"
 	"github.com/syrlramadhan/etika-reservation-api/controller"
@@ -11,15 +10,31 @@ import (
 	"github.com/syrlramadhan/etika-reservation-api/service"
 )
 
+func withCORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
 func main() {
 	log.Println("Etika Reservation API")
+
 	db := config.ConnectDB()
 	repo := repository.NewReservationRepository(db)
 	svc := service.NewReservationService(repo)
 	ctrl := controller.NewReservationController(svc)
 
-	http.HandleFunc("/api/reservations", ctrl.CreateReservation)
-	http.HandleFunc("/api/reservations/by-date", ctrl.GetReservationsByDate)
+	http.HandleFunc("/api/reservations", withCORS(ctrl.CreateReservation))
+	http.HandleFunc("/api/reservations/by-date", withCORS(ctrl.GetReservationsByDate))
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
