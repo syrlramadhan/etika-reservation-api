752336da2a3638205f9c3b9c15bbeedd23450fb9
syrlramadhan <syahrul0422@gmail.com>
2025-06-21 02:50:36 +0800

    update

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func withCORS(next http.HandlerFunc) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Origin", "http://103.84.207.100")
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
